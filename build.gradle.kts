ce6c25bd14bd0a161892f5f1e4a59498ec9b3c8a
Vincent Girard <Vinccool96@gmail.com>
2022-10-15 13:05:06 -0400

    Upgraded Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-val kotlinVersion = "1.7.20-Beta"
+val kotlinVersion = "1.7.20"
 
 plugins {
-    kotlin("multiplatform") version "1.7.20-Beta"
+    kotlin("multiplatform") version "1.7.20"
 }
 
 group = "io.github.vinccool96.observable"
